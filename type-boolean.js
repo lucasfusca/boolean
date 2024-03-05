af661d535811fff76980bb0e24cd9680f849fce0
lucas/fusca <lucas.mateus.pereira26@gmail.com>
2024-03-05 17:10:40 -0300

    arquivos javascript

---
+++ type-boolean.js
@@ -0,0 +1,36 @@
+// boolean
+// true -> verdadeiro
+// false -> falso
+
+
+
+
+const primeiroNumero = 10;
+const segundoNumero = 500;
+const cadastroAtivo = true;
+
+// console.log(primeiroNumero === segundoNumero) ;
+
+const texto1 = "alura";
+const texto2 = "alura";
+
+console.log(texto1 === texto2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ type-string.js
@@ -0,0 +1,14 @@
+const texto1 = "ola, mundo!";
+const texto2 = 'ola, mundo!';
+const senha = "senhaSuperSecreta6969!";
+const StringDeNumero = "34567";
+
+const citacao = "Meu nome é ";
+const meuNome = "charly chaplin";
+console.log(citacao)
+
+// concatenação (+)
+
+console.log(citacao + meuNome)
+
+//template string ou temple literal
